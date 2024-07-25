5353f198f2409b8f86ec8ff4dc35d4cb4278c48b
huangyumin <notcorn@gmail.com>
2024-07-25 15:02:06 +0800

    feat: add order example

---
+++ src/test/java/order/OrderServiceTest.java
@@ -0,0 +1,15 @@
+package order;
+
+
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    @Test
+    public void syncbookorders_3_orders_only_2_book_order() {
+        // hard to isolate dependency to unit test
+
+        //OrderService target = new OrderService();
+        //target.SyncBookOrders();
+    }
+}
